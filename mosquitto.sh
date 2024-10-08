20b8fd33ddcdb395303fb8298a1970a96de6a8b4
Sébastien Dominguez <SebDominguez@users.noreply.github.com>
2024-10-08 19:54:05 +0200

    Avoid using the -c option to prevent overwriting the existing passwd file. Also, forced docker exec to run as root to prevent Mosquitto warnings about incorrect file ownership.

--- mosquitto.sh
+++ mosquitto.sh
@@ -31,10 +31,17 @@ COMPOSE_FILE=$DIR/docker-compose.yml
 LOCAL_CONFIG=./config/
 CONTAINER_CONFIG=/mosquitto/config/
 
-PASSWD_FILE_NAME=passwordfile
+PASSWD_FILE_NAME=passwd
 LOCAL_PASSWORD_FILE="${LOCAL_CONFIG}${PASSWD_FILE_NAME}"
 CONTAINER_PASSWORD_FILE="${CONTAINER_CONFIG}${PASSWD_FILE_NAME}"
 
+if [ ! -f $LOCAL_PASSWORD_FILE ]; then
+    echo "Password file not found. Creating $LOCAL_PASSWORD_FILE"
+
+    touch $LOCAL_PASSWORD_FILE
+
+    echo "Blank password file created"
+fi
 
 # Check if docker-compose exist. If it doesnt then we use docker compose instead
 if command -v docker-compose &> /dev/null
@@ -71,6 +78,7 @@ function update() {
 }
 
 function createUser(){
+    docker exec --user root mosquitto chown root:root $CONTAINER_PASSWORD_FILE
     read -p "Enter the username for mosquitto_passwd: " username
     # Check if the username is not empty
     if [[ -z "$username" ]]; then
@@ -87,10 +95,11 @@ function createUser(){
         exit 1
     fi
     # Use mosquitto_passwd to add the username and password in batch mode
-    docker exec -it mosquitto mosquitto_passwd -b -c $CONTAINER_PASSWORD_FILE $username $password 
+    docker exec --user root mosquitto mosquitto_passwd -b $CONTAINER_PASSWORD_FILE $username $password 
 }
 
 function listUsers(){
+    docker exec --user root mosquitto chown root:root $CONTAINER_PASSWORD_FILE
     # Check if the password file exists
     if [[ ! -f "$LOCAL_PASSWORD_FILE" ]]; then
         echo "Error: Password file does not exist at $LOCAL_PASSWORD_FILE"
@@ -98,11 +107,12 @@ function listUsers(){
     fi
     # List the users by displaying the first field (username) of each line
     echo "Mosquitto users:"
-    docker exec -it mosquitto /usr/bin/cut -d ':' -f 1 "$CONTAINER_PASSWORD_FILE"
+    docker exec --user root mosquitto /usr/bin/cut -d ':' -f 1 "$CONTAINER_PASSWORD_FILE"
 }
 
 function deleteUser(){
-    docker exec -it mosquitto mosquitto_passwd -D $CONTAINER_PASSWORD_FILE $1
+    docker exec --user root mosquitto chown root:root $CONTAINER_PASSWORD_FILE
+    docker exec --user root mosquitto mosquitto_passwd -D $CONTAINER_PASSWORD_FILE $1
     # Check the exit code
     if [[ $? -ne 0 ]]; then
         echo "Error: Failed to delete user $1."
